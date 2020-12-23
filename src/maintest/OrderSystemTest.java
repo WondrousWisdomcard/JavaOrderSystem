7a407e8e4e2bef2d13e86b8c67c0483d31fb82cc
WondrousWisdomcard <1772794829@qq.com>
2020-12-23 13:14:34 +0800

    !

--- src/maintest/OrderSystemTest.java
+++ src/maintest/OrderSystemTest.java
@@ -12,17 +12,19 @@ public class OrderSystemTest {
 
     public static void main(String[] args) {
 
-    OperatorInterface t2 = new OperatorInterface();
+    OperatorInterface Client = new OperatorInterface();
 
-    ServerOperatorInterface t = new ServerOperatorInterface();
+    ServerOperatorInterface Server  = new ServerOperatorInterface();
 
-    t2.ep.confirm_btn.addActionListener(new ActionListener() {
+    Client.ep.confirm_btn.addActionListener(new ActionListener() { //确认订单Button的监听器
 			public void actionPerformed(ActionEvent e) {
-        
-        PurchaseCuisines plist = t2.getMessage();
-        t.newTask(plist);
 
-				JDialog result = new JDialog(t2, "下单反馈", true);
+        ///////////
+        PurchaseCuisines plist = Client.getMessage(); //获得用户界面的点菜反馈
+        Server.newTask(plist); //在服务端界面显示
+        //////////
+        
+				JDialog result = new JDialog(Client, "下单反馈", true);
 				result.setBounds(600, 400, 200, 100);
 				result.setLayout(new FlowLayout(1));
 				JLabel j = new JLabel("下单成功!");
